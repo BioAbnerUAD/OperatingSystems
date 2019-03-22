8c081424e4b9103495f478ed5b749214f4204729
BioAbner <bioabner@hotmail.com>
2019-03-22 17:05:42 -0600

    try again 1st code

--- philosophers.cpp
+++ philosophers.cpp
@@ -1,135 +1,125 @@
-
-#include <stdio.h>
+#include <algorithm>
+#include <array>
+#include <atomic>
+#include <chrono>
+//We are using only standard library, so snprintf instead of Boost::Format
+#include <cstdio>
+#include <iostream>
+#include <mutex>
+#include <random>
 #include <string>
 #include <thread>
-#include <semaphore.h>
 
-#define N 5
+std::mutex cout_mutex;
 
-enum philState
+struct Fork
 {
-  EATING = 0,
-  HUNGRY,
-  THINKING,
+  std::mutex mutex;
 };
 
-#define LEFT (phnum + 4) % N 
-#define RIGHT (phnum + 1) % N 
+struct Dinner
+{
+  std::atomic<bool> ready{ false };
+  std::array<Fork, 5> forks;
+  ~Dinner() { std::cout << "Dinner is over"; }
+};
 
-int state[N];
-int phil[N] = { 0, 1, 2, 3, 4 };
-std::string philName[N] = { "Kierkegaard",
-                            "Nietzsche",
-                            "Socrates",
-                            "Descartes",
-                            "Don Ramon" };
+class Philosopher
+{
+  std::mt19937 rng{ std::random_device {}() };
 
-sem_t mutex;
-sem_t semaphore[N];
+  const std::string name;
+  const Dinner& dinner;
+  Fork& left;
+  Fork& right;
+  std::thread worker;
 
-void test(int phnum) {
-  if (state[phnum] == HUNGRY
-      && state[LEFT] != EATING
-      && state[RIGHT] != EATING) {
-    // state that eating 
-    state[phnum] = EATING;
-
-    printf((philName[phnum] + " takes fork %d and %d\n").c_str(), LEFT + 1, phnum + 1);
-
-    printf((philName[phnum] + " is Eating\n").c_str());
-
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-
-    // sem_post(&S[phnum]) has no effect 
-    // during takefork 
-    // used to wake up hungry philosophers 
-    // during putfork 
-    sem_post(&semaphore[phnum]);
+  void live();
+  void dine();
+  void ponder();
+public:
+  Philosopher(std::string name_, const Dinner& dinn, Fork& l, Fork& r)
+    : name(std::move(name_)), dinner(dinn), left(l), right(r), worker(&Philosopher::live, this) {}
+  ~Philosopher() {
+    worker.join();
+    std::lock_guard<std::mutex>  cout_lock(cout_mutex);
+    std::cout << name << " went to sleep." << std::endl;
   }
+};
+
+void Philosopher::live() {
+  while (not dinner.ready)
+    ; //You spin me right round, baby, right round...
+  do {//Aquire forks first
+      //lock uses deadlock prevention mechanism to acquire mutexes safely
+    std::lock(left.mutex, right.mutex);
+    dine(); //Dine adopts lock on forks and releases them
+    if (not dinner.ready) break;
+    ponder();
+  } while (dinner.ready);
 }
 
-// take up chopsticks 
-void take_fork(int phnum) {
+void Philosopher::dine() {
+  std::lock_guard<std::mutex>  left_lock(left.mutex, std::adopt_lock);
+  std::lock_guard<std::mutex> right_lock(right.mutex, std::adopt_lock);
 
-  sem_wait(&mutex);
+  thread_local std::array<const char*, 3> foods{ {"chicken", "rice", "soda"} };
+  thread_local std::array<const char*, 3> reactions{ {
+      "I like this %s!", "This %s is good.", "Mmm, %s..."
+  } };
+  thread_local std::uniform_int_distribution<> dist(1, 6);
+  std::shuffle(foods.begin(), foods.end(), rng);
+  std::shuffle(reactions.begin(), reactions.end(), rng);
 
-  // state that hungry 
-  state[phnum] = HUNGRY;
-
-  printf((philName[phnum] + " is Hungry\n").c_str());
-
-  // eat if neighbours are not eating 
-  test(phnum);
-
-  sem_post(&mutex);
-
-  // if unable to eat wait to be signalled 
-  sem_wait(&semaphore[phnum]);
-
-  std::this_thread::sleep_for(std::chrono::milliseconds(1));
-}
-
-// put down chopsticks 
-void put_fork(int phnum) {
-
-  sem_wait(&mutex);
-
-  // state that thinking 
-  state[phnum] = THINKING;
-
-  printf((philName[phnum] + " putting fork %d and %d down\n").c_str(), LEFT + 1, phnum + 1);
-  printf((philName[phnum] + " is thinking\n").c_str());
-
-  test(LEFT);
-  test(RIGHT);
-
-  sem_post(&mutex);
-}
-
-void* philospher(void* num) {
-
-  while (true) {
-
-    int* i = (int*)num;
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(1));
-
-    take_fork(*i);
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(1));
-
-    put_fork(*i);
+  if (not dinner.ready) return;
+  {
+    std::lock_guard<std::mutex>  cout_lock(cout_mutex);
+    std::cout << name << " started eating." << std::endl;
   }
+  constexpr size_t buf_size = 64;
+  char buffer[buf_size];
+  for (int i = 0; i < 3; ++i) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(dist(rng) * 50));
+    snprintf(buffer, buf_size, reactions[i], foods[i]);
+    std::lock_guard<std::mutex>  cout_lock(cout_mutex);
+    std::cout << name << ": " << buffer << std::endl;
+  }
+  std::this_thread::sleep_for(std::chrono::milliseconds(dist(rng)) * 50);
+  std::lock_guard<std::mutex>  cout_lock(cout_mutex);
+  std::cout << name << " finished and left." << std::endl;
+}
+
+void Philosopher::ponder() {
+  static constexpr std::array<const char*, 5> topics{ {
+      "politics", "art", "meaning of life", "source of morality", "how many straws makes a bale"
+  } };
+  thread_local std::uniform_int_distribution<> wait(1, 6);
+  thread_local std::uniform_int_distribution<> dist(0, topics.size() - 1);
+  while (dist(rng) > 0) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(wait(rng) * 150));
+    std::lock_guard<std::mutex>  cout_lock(cout_mutex);
+    std::cout << name << " is pondering about " << topics[dist(rng)] << '.' << std::endl;
+    if (not dinner.ready) return;
+  }
+  std::this_thread::sleep_for(std::chrono::milliseconds(wait(rng) * 150));
+  std::lock_guard<std::mutex>  cout_lock(cout_mutex);
+  std::cout << name << " is hungry again!" << std::endl;
 }
 
 int main() {
-
-  int i;
-  std::thread* thread_id[N];
-
-  // initialize the semaphores 
-  sem_init(&mutex, 0, 1);
-
-  for (i = 0; i < N; i++) {
-    sem_init(&semaphore[i], 0, 0);
-  }
-
-  for (i = 0; i < N; i++) {
-
-    // create philosopher processes 
-    thread_id[i] = new std::thread(philospher, &phil[i]);
-
-    printf((philName[i] + " is thinking\n").c_str());
-  }
-
-  for (i = 0; i < N; i++) {
-    thread_id[i]->join();
-  }
-
-
-  for (i = 0; i < N; i++) {
-    delete thread_id[i];
-  }
-
-  return 0;
+  Dinner dinner;
+  std::array<Philosopher, 5> philosophers{ {
+          {"Aristotle", dinner, dinner.forks[0], dinner.forks[1]},
+          {"Kant",      dinner, dinner.forks[1], dinner.forks[2]},
+          {"Spinoza",   dinner, dinner.forks[2], dinner.forks[3]},
+          {"Marx",      dinner, dinner.forks[3], dinner.forks[4]},
+          {"Russell",   dinner, dinner.forks[4], dinner.forks[0]},
+  } };
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+  std::cout << "Dinner started!" << std::endl;
+  dinner.ready = true;
+  std::this_thread::sleep_for(std::chrono::seconds(5));
+  dinner.ready = false;
+  std::lock_guard<std::mutex>  cout_lock(cout_mutex);
+  std::cout << "It is dark outside..." << std::endl;
 }
